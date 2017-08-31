ddbdbaf6feeb50448adc0663742d24324c64b9ce
Kananek Thongkam <info.dvgamer@gmail.com>
2017-08-31 18:10:52 +0700

    fixed logs

--- slack-webhook/index.js
+++ slack-webhook/index.js
@@ -1,12 +1,12 @@
 const request = require('request-promise')
 
 module.exports = {
-	logs: message => {
+	logs: (name, message) => {
 		return request({
 			method: 'POST',
 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 			url: 'https://hooks.slack.com/services/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',
-			formData: { payload: JSON.stringify({ text: message }) }
+			formData: { payload: JSON.stringify({ text: message, username: name }) }
 		})
 	},
 	hook: (name, message) => {
