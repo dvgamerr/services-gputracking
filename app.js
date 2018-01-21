339b392075369678d71bb0fe3826b294d959986c
Kananek Thongkam <info.dvgamer@gmail.com>
2018-01-21 13:24:56 +0700

    gpu tracking with open monitor

--- app.js
+++ app.js
@@ -9,34 +9,43 @@ let main = async () => {
     url: `http://192.168.1.3:8085/data.json`,
     json: true
   })
-  let MinerName = data.Children[0].Text
+  let miner = {
+    name: data.Children[0].Text,
+    created: new Date(),
+    device: []
+  }
   let items = data.Children[0].Children
   for (let i = 0; i < items.length; i++) {
     if (items[i].Text.indexOf('NVIDIA') > -1) {
-      let device = { name: items[i].Text }
+      let device = {
+        name: items[i].Text
+      }
       for (let l = 0; l < items[i].Children.length; l++) {
         let gpu = items[i].Children[l]
         if (gpu.Text === 'Temperatures') {
-          let temperature = (gpu.Children || [{}])[0]
+          device.temperature = (gpu.Children || [{}])[0].Value
         } else if (gpu.Text === 'Fans') {
-          let fan = (gpu.Children || [{}])[0]
+          device.fan = (gpu.Children || [{}])[0].Value
         } else if (gpu.Text === 'Controls') {
-          let control = (gpu.Children || [{}])[0]
+          device.control = (gpu.Children || [{}])[0].Value
         } else if (gpu.Text === 'Load') {
-          let load = (gpu.Children || [{}])[0]
+          device.load = (gpu.Children || [{}])[0].Value
         }
       }
-      console.log(`${moment().format('YYYMMDD HH:mm:ss')} ${device.name}: ${device.temperature.Value}`)
+      miner.device.push(device)
     }
   }
-
-
-
-
-
+  console.log(miner)
+  console.log('----------------------------------------------------')
 }
 
-setInterval(main, 1000)
+setInterval(() => {
+  main().then(() => {
+
+  }).catch(ex => {
+    console.log(ex.message || ex)
+  })
+}, 1000)
 // r.connect({ host: 'aws.touno.co', port: 6511 }, function(err, conn) {
 // r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
 // 	conn.use('miner')
