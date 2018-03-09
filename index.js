f5b6bf12e8133fc0f615656dadc24b465fe11174
Kananek T <info.dvgamer@gmail.com>
2018-03-10 00:32:54 +0700

    fixed mongodb and update travis test

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const { debug } = require('touno.io').Variable
 const { Raven } = require('touno.io')
 const { LINE } = require('touno.io').Notify
+const { MongooseOpen, MongooseClose, Miner } = require('touno.io/mongodb')
 const { r, rdbConnection } = require('touno.io/rethinkdb')
 const cron = require('cron')
 const os = require('os')
 const util = require('util')
 const nvsmi = require('./nvidia-smi')
 const exec = util.promisify(require('child_process').exec)
-// const sms = require('sms-gateway-nodejs')('info.dvgamer@gmail.com', 'dvg7po8ai')
 let GPU_MAX = process.env.GPU_MAX || 1
 let IsShutdown = false
 let GPU = []
 
-// sms.device.listOfDevices(1).then(async res => {
-//   conosle.log('--- listOfDevices')
-//   console.log(res)
-//   let msg = await sms.message.sendMessageToNumber('81450', '+66970347607', 'Hello world :)')
-//   conosle.log('--- sendMessageToNumber')
-//   console.log(msg)
-// }).then(ex => {
-//   console.log(`ERROR::${ex.message}`)
-// })
-
-// sms.message.listOfMessages(1).then((response) => {
-//   console.log(response)
-// }).catch((error) => {
-//   console.log('error', error)
-// })
-console.log(`[GPU] RethinkDB Connectioning...`)
-rdbConnection().then(conn => {
+console.log(`[GPU] RethinkDB Connecting...`)
+rdbConnection().then(async conn => {
+  console.log(`[GPU] MongoDB Connecting...`)
+  await MongooseOpen({ user: 'admin', pass: 'ar00t-touno', dbname: 'db_touno' })
   let wait = 10
   let sendRestart = async () => {
     const { stderr } = await exec(`shutdown -r -t ${wait}`)
@@ -36,6 +23,7 @@ rdbConnection().then(conn => {
   }
 
   let gpuUploadData = async (i, smi) => {
+    if (IsShutdown) process.exit(0)
     if (smi.alive) {
       if (!GPU[i]) {
         console.log(`[GPU] Processing ${smi.index}:${smi.name} ${smi.bus_id}.`)
@@ -48,9 +36,16 @@ rdbConnection().then(conn => {
           bus_id: smi.bus_id,
           updated: smi.date
         }
-      } else {
+        if (await Miner.findOne({ gpu_id: smi.uuid })) {
+          await Miner.update({ gpu_id: smi.uuid }, { $set: { updated: GPU[i].updated } })
+        } else {
+          await new Miner(GPU[i]).save()
+        }
+      } else if (smi.date - GPU[i].updated > 60000 * 5) {
         GPU[i].updated = smi.date
+        await Miner.update({ gpu_id: smi.uuid }, { $set: { updated: GPU[i].updated } })
       }
+
       let result = await r.db('miner').table('gpu_01').insert({
         gpu_id: smi.uuid,
         created: smi.date,
@@ -66,7 +61,9 @@ rdbConnection().then(conn => {
     } else {
       if (!IsShutdown) {
         LINE.Miner(`การ์ดจอ ${smi.index}:${smi.name} ดับ\nบนเครื่อง ${os.hostname()} ซึ่งกำลังรีสตาร์ทใน ${wait} วินาที.`)
-        if (!debug) sendRestart()
+        if (!debug) await sendRestart()
+        await MongooseClose()
+        await conn.close()
       }
       IsShutdown = true
     }
@@ -95,7 +92,7 @@ rdbConnection().then(conn => {
     start: true,
     timeZone: 'Asia/Bangkok'
   })
-  console.log(`[GPU] Add Tasks ${job.running ? 'started' : 'stoped'}`)
+  console.log(`[GPU] Tasks ${job.running ? 'started' : 'stoped'}`)
 }).catch(ex => {
   Raven(ex)
   process.exit(0)
