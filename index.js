65c6c0fcd8695121e452cafe5a790a1d2c16d221
Kananek T <info.dvgamer@gmail.com>
2018-03-10 12:30:22 +0700

    check time an update log

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ rdbConnection().then(async conn => {
         } else {
           await new Miner(GPU[i]).save()
         }
-      } else if (smi.date - GPU[i].updated > 60000 * 5) {
+      } else if (smi.date - GPU[i].updated > 60000 * 3) {
         GPU[i].updated = smi.date
         await Miner.update({ gpu_id: smi.uuid }, { $set: { updated: GPU[i].updated } })
       }
@@ -92,113 +92,8 @@ rdbConnection().then(async conn => {
     start: true,
     timeZone: 'Asia/Bangkok'
   })
-  console.log(`[GPU] Tasks ${job.running ? 'started' : 'stoped'}`)
+  console.log(`[GPU] Purge data Tasks ${job.running ? 'started' : 'stoped'}`)
 }).catch(ex => {
   Raven(ex)
   process.exit(0)
 })
-
-// GPUMINER-01 Query {
-//   index: '2',
-//   uuid: 'GPU-b9e798e8-9ec2-e1f7-3467-4885adf3f6e2',
-//   state: 'P2',
-//   date: moment("2018-03-05T21:32:01.442"),
-//   name: 'GeForce GTX 1080 Ti',
-//   bus_id: '00000000:09:00.0',
-//   temp: '71',
-//   ugpu: '100 %',
-//   umemory: '72 %',
-//   power: { draw: '252.03 W', limit: '250.00 W' },
-//   clocks: '5005 MHz',
-//   fan: '81 %',
-//   memory: { total: '11264 MiB', free: '3497 MiB', used: '7767 MiB' } }
-
-// const request = require('request-promise')
-// const moment = require('moment')
-
-// const dbConnection = () => {
-//   let connection = {
-//     host: process.env.RETHINKDB_HOST,
-//     port: process.env.RETHINKDB_PORT,
-//     db: 'miner'
-//   }
-//   return new Promise((resolve, reject) => {
-//     rdb.connect(connection, (err, conn) => {
-//       if (err) reject(err); else resolve(conn)
-//     })
-//   })
-// }
-
-// const dbInsert = (conn, table, data) => {
-//   return new Promise((resolve, reject) => {
-//     rdb.table(table).insert(data).run(conn, (err, result) => {
-//       if (err) reject(err); else resolve(result)
-//     })
-//   })
-// }
-
-// const dbDelete = (conn, table, filter) => {
-//   return new Promise((resolve, reject) => {
-//     rdb.table(table).filter(filter).delete().run(conn, (err, result) => {
-//       if (err) reject(err); else resolve(result)
-//     })
-//   })
-// }
-
-// if (!process.env.RETHINKDB_HOST) throw new Error(`Required 'RETHINKDB_HOST' environment.`)
-// if (!process.env.RETHINKDB_PORT) throw new Error(`Required 'RETHINKDB_PORT' environment.`)
-
-// let main = async (conn) => {
-//   let data = await request({
-//     url: `http://${process.env.MONITOR_HOST ? process.env.MONITOR_HOST : '127.0.0.1'}${process.env.MONITOR_PORT ? `:${process.env.MONITOR_PORT}` : ':8085'}/data.json`,
-//     json: true
-//   })
-//   let miner = {
-//     name: data.Children[0].Text,
-//     created: new Date(),
-//     device: []
-//   }
-//   let items = data.Children[0].Children
-//   for (let i = 0; i < items.length; i++) {
-//     if (items[i].Text.indexOf('NVIDIA') > -1) {
-//       let device = {
-//         name: items[i].Text
-//       }
-//       for (let l = 0; l < items[i].Children.length; l++) {
-//         let gpu = items[i].Children[l]
-//         if (gpu.Text === 'Temperatures') {
-//           device.temperature = (gpu.Children || [{}])[0].Value
-//         } else if (gpu.Text === 'Fans') {
-//           device.fan = (gpu.Children || [{}])[0].Value
-//         } else if (gpu.Text === 'Controls') {
-//           device.control = (gpu.Children || [{}])[0].Value
-//         } else if (gpu.Text === 'Load') {
-//           device.load = (gpu.Children || [{}])[0].Value
-//         }
-//       }
-//       miner.device.push(device)
-//     }
-//   }
-//   if (process.env.NODE_ENV !== 'development') {
-//     await dbInsert(conn, 'gpu', miner)
-//   } else {
-//     console.log(`${moment(miner.created).format('YYYY-MM-DD HH:mm:ss')} | ${miner.device[0].temperature} ${miner.device[1].temperature} ${miner.device[2].temperature} ${miner.device[3].temperature} ${miner.device[4].temperature}`)
-//   }
-// }
-
-// console.log(`[hardware-monitor] connecting '${process.env.RETHINKDB_HOST}'...`)
-// dbConnection().then(async conn => {
-//   console.log(`[hardware-monitor] connected, monitor started`)
-//   setInterval(async () => {  await main(conn).catch(RavenException) }, 1000)
-// }).catch(RavenException)
-
-// let jobDelete = new cron.CronJob({
-//   cronTime: '0 0 * * *',
-//   onTick: async () => {
-//     let conn = await dbConnection()
-//     console.log(`[hardware-monitor] rethinkdb remove colletion ${jobDelete.running ? 'complated' : 'stoped'}.`)
-//     await dbDelete(conn, 'gpu', item => rdb.now().sub(item('created')).gt(60 * 60 * 24 * 365)).catch(RavenException)
-//   },
-//   start: true,
-//   timeZone: 'Asia/Bangkok'
-// })
